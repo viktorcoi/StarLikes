56cbdf385ddabef02614ec4f0bb79804eeb9db73
vitek <marandin5913@mail.ru>
2023-06-14 11:58:00 +0300

    git-pages

---
+++ .next/static/fGAmi2bwPwRhFtnIiBru_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb2bc37cc6948f57.css","static/chunks/pages/index-56d8df8188a32bb9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
